7583df1e791d96ba0da382300871b2cb56d605d9
VolkerFelix <volker.leukhardt@gmail.com>
2023-11-27 21:12:30 +0000

    Cleaning up a bit

--- src/pong.py
+++ src/pong.py
@@ -5,61 +5,70 @@ import numpy as np
 import gym
 import pickle
 
-# hyperparameters
-H = 200 # number of hidden layer neurons
-batch_size = 10 # every how many episodes to do a param update?
-learning_rate = 0.001
-gamma = 0.99 # discount factor for reward
-decay_rate = 0.99 # decay factor for RMSProp leaky sum of grad^2
-resume = True # resume from previous checkpoint?
-render = True
+# Hyperparameters
+HIDDEN_NEURONS = 200 # Number of hidden layer neurons
+BATCH_SIZE = 10 # Param update after how many episodes?
+LEARNING_RATE = 0.001
+GAMMA = 0.99 # Discount factor for reward
+DECAY_RATE = 0.99 # Decay factor for RMSProp leaky sum of grad^2
 
-# model initialization
-D = 80 * 80 # input dimensionality: 80 x 80 grid
+# Configuration
+RESUME = True # Resume from previous checkpoint?
+RENDER = True
+CROPPED_FRAME_DIM = 80 * 80
+CROP_LEFT = 35 # Pixels to be cropped counting from the left
+CROP_RIGHT = 15 # Pixels to be cropped counting from the right
 
-if resume:
-  model = pickle.load(open('model/save.p', 'rb'))
-else:
+def check_dimensions_match():
+  assert (210 - CROP_LEFT - CROP_RIGHT) == 160, "The cropped frame is not 160px wide."
+
+def initialize_model(f_nr_hidden_neurons: int, f_nr_inputs: int) -> dict:
+  """Init the model using "Xavier" initialization"""
   model = {}
-  model['W1'] = np.random.randn(H,D) / np.sqrt(D) # "Xavier" initialization
-  model['W2'] = np.random.randn(H) / np.sqrt(H)
+  model['W1'] = np.random.randn(f_nr_hidden_neurons, f_nr_inputs) / np.sqrt(f_nr_inputs)
+  model['W2'] = np.random.randn(f_nr_hidden_neurons) / np.sqrt(f_nr_hidden_neurons)
+  return model
 
-# Creates a dict with key, value pairs  
-grad_buffer = { key : np.zeros_like(value) for key,value in model.items() } # update buffers that add up gradients over a batch
-rmsprop_cache = { key : np.zeros_like(value) for key,value in model.items() } # rmsprop memory
+def sigmoid(f_x: float) -> float:
+  """Sigmoid function"""
+  return 1.0 / (1.0 + np.exp(-f_x)) # squash values to [0, 1]
 
-def sigmoid(f_x): 
-  return 1.0 / (1.0 + np.exp(-f_x)) # sigmoid "squashing" function to interval [0,1]
-
-def prepro(f_field_input):
-  """ pre-process 210x160x3 uint8 frame into 6400 (80x80) 1D float vector """
-  cropped_field = f_field_input[35:195] # crop
-  cropped_field_down = cropped_field[::2,::2,0] # downsample by factor of 2
-  # Keep in mind that the input will be a numpy array,
-  # meaning that the == allows to do an element-wise comparison with a scalar
+def preprocess_frame(f_frame_input: np.array) -> np.array:
+  """Pre-process a 210 x 160 x 3 rgb frame into 6400 (80 x 80) 1D float vector"""
+  cropped_field = f_frame_input[CROP_LEFT : (210 - CROP_RIGHT)] # crop width
+  cropped_field_down = cropped_field[::2, ::2, 0] # downsample by factor of 2 and take only the "R" value
   cropped_field_down[cropped_field_down == 144] = 0 # erase background (background type 1)
   cropped_field_down[cropped_field_down == 109] = 0 # erase background (background type 2)
   cropped_field_down[cropped_field_down != 0] = 1 # everything else (paddles, ball) just set to 1
   return cropped_field_down.astype(float).ravel()
 
-def policy_forward(x):
-  """forward path through the neural net which serves as the policy"""
-  h = np.dot(model['W1'], x)
+def policy_forward(f_x: np.array) -> (float, np.array):
+  """Forward path through the neural net, which serves as the policy"""
+  h = np.dot(model['W1'], f_x)
   h[h<0] = 0 # ReLu activation
-  logp = np.dot(model['W2'], h)
-  p = sigmoid(logp) # Prob between 0 and 1
-  return p, h # return probability of taking action 2, and hidden state
+  logp = np.dot(model['W2'], h) # Log probability
+  p = sigmoid(logp) # Probability between 0 and 1
+  return p, h # return probability of taking action 2 (UP), and hidden state
+
+if RESUME:
+  model = pickle.load(open('model/save.p', 'rb'))
+else:
+  model = initialize_model(HIDDEN_NEURONS, CROPPED_FRAME_DIM)
+
+# Creates a dict with key, value pairs  
+grad_buffer = { key : np.zeros_like(value) for key, value in model.items() } # update buffers that add up gradients over a batch
+rmsprop_cache = { key : np.zeros_like(value) for key,value in model.items() } # rmsprop memory
 
 def discounted_rewards(r):
   """calculates the discounted rewards of an episode backwards in time"""
-  # G_t+1 = R + gamma * G_t
+  # G_t+1 = R + GAMMA * G_t
   # the reason to calculate it backwards in time is that it enables us to use an iterative algo
   discounted_r = np.zeros_like(r).astype('float64')
   summed_up_r = 0
   for t in reversed(range(0, r.size)):
     if r[t] != 0:
       summed_up_r = 0 # One player has won, so we need to reset
-    summed_up_r = r[t] + gamma * summed_up_r
+    summed_up_r = r[t] + GAMMA * summed_up_r
     discounted_r[t] = summed_up_r
   return discounted_r
 
@@ -86,120 +95,126 @@ def policy_backward(epx, eph, edplogp):
 
 env = gym.make('ALE/Pong-v5', render_mode='human')
 
-observation = env.reset()
+print(env.metadata)
 
-observation = observation[0]
+observation, info = env.reset()
+
+#observation = observation[0]
 prev_x = None # used for computing the difference frame
 xs,hs,dlogps,drs = [],[],[],[]
 running_reward = None
 reward_sum = 0
 episode_number = 0
 
-while True:
-  if render:
-    env.render()
+if __name__ == "__main__":
 
-  # preprocess the observation, set input to network to be difference image
-  cur_x = prepro(observation)
-  x = cur_x - prev_x if prev_x is not None else np.zeros(D)
-  prev_x = cur_x
+  check_dimensions_match()
 
-  # forward the policy network and sample an action from the returned probability  
-  aprob, h = policy_forward(x)
-  action = 2 if np.random.uniform() < aprob else 3 # roll the dice!
+  while True:
+    if RENDER:
+      env.render()
 
-  # record various intermediates (needed later for backprop)
-  xs.append(x) # observation
-  hs.append(h) # hidden state
+    # preprocess the observation, set input to network to be difference image
+    cur_x = preprocess_frame(observation)
+    x = cur_x - prev_x if prev_x is not None else np.zeros(CROPPED_FRAME_DIM)
+    prev_x = cur_x
 
-  # Background:
-  # Classic policy gradient algo = REINFORCE
-  # gradient(Expectaion[r(tau)] = Expectation[(SUM(G_t * gradient * log * PI_teta(a_t|s_t)))]
-  # With PI_teta: Policy PI with its params teta
-  # PI_teta is the output of the 2-layer policy network: aprob
-  # Hint: In order to get rid of the expectation, we sample over a large number of trajectories
-  # and average them out. This technique is called Markov Chain Monte-Carlo.
-  #
-  # Calculate the gradient * log * PI_teta:
-  # aprob = sigmoid(in)
-  # if action == 2:
-  # logprob = log(aprob)
-  # dlogprob/daprob = dlog(aprob)/daprob * daprob/din
-  # = 1/aprob * d_aprob/d_in = 1/aprob * sigmoid(in)(1-sigmoid(in))
-  # = 1 - aprob
-  # if action == 3:
-  # logprob = log(1-aprob)
-  # dlogprob/daprob = dlog(1-aprob)/daprob * daprob/din
-  # = -1/(1-aprob) * sigmoid(in)(1-sigmoid(in))
-  # = -aprob/(1-aprob)*(1-aprob) = -aprob = 0 - aprob
-  y = 1 if action == 2 else 0 # fake label
+    # forward the policy network and sample an action from the returned probability  
+    aprob, h = policy_forward(x)
+    action = 2 if np.random.uniform() < aprob else 3 # roll the dice!
 
-  dlogps.append(y - aprob) # grad that encourages the action that was taken to be taken
+    # record various intermediates (needed later for backprop)
+    xs.append(x) # observation
+    hs.append(h) # hidden state
 
-  # step the environment and get new measurements
-  observation, reward, done, truncated, info = env.step(action)
+    # Background:
+    # Classic policy gradient algo = REINFORCE
+    # gradient(Expectaion[r(tau)] = Expectation[(SUM(G_t * gradient * log * PI_teta(a_t|s_t)))]
+    # With PI_teta: Policy PI with its params teta
+    # PI_teta is the output of the 2-layer policy network: aprob
+    # Hint: In order to get rid of the expectation, we sample over a large number of trajectories
+    # and average them out. This technique is called Markov Chain Monte-Carlo.
+    #
+    # Calculate the gradient * log * PI_teta:
+    # aprob = sigmoid(in)
+    # if action == 2:
+    # logprob = log(aprob)
+    # dlogprob/daprob = dlog(aprob)/daprob * daprob/din
+    # = 1/aprob * d_aprob/d_in = 1/aprob * sigmoid(in)(1-sigmoid(in))
+    # = 1 - aprob
+    # if action == 3:
+    # logprob = log(1-aprob)
+    # dlogprob/daprob = dlog(1-aprob)/daprob * daprob/din
+    # = -1/(1-aprob) * sigmoid(in)(1-sigmoid(in))
+    # = -aprob/(1-aprob)*(1-aprob) = -aprob = 0 - aprob
+    y = 1 if action == 2 else 0 # fake label
 
-  reward_sum += reward
+    dlogps.append(y - aprob) # grad that encourages the action that was taken to be taken
 
-  drs.append(reward)
+    # step the environment and get new measurements
+    observation, reward, done, truncated, info = env.step(action)
 
-  if done:
-    # an episode has finished (= one player reached a score of 21)
-    episode_number += 1
+    reward_sum += reward
 
-    # stack together all inputs, hidden states, action gradients, and rewards for this episode
-    epx = np.vstack(xs)
-    eph = np.vstack(hs)
-    epdlogp = np.vstack(dlogps)
-    epr = np.vstack(drs)
-    xs,hs,dlogps,drs = [],[],[],[] # reset array memory
+    drs.append(reward)
 
-    # compute the discounted reward
-    discounted_epr = discounted_rewards(epr)
-    # standardize the rewards to be unit normal (helps control the gradient estimator variance)
-    discounted_epr -= np.mean(discounted_epr)
-    discounted_epr /= np.std(discounted_epr)
+    if done:
+      # an episode has finished (= one player reached a score of 21)
+      episode_number += 1
 
-    epdlogp *= discounted_epr # modulate the gradient with advantage (PG magic happens right here.)
-    grad = policy_backward(epx, eph, epdlogp)
+      # stack together all inputs, hidden states, action gradients, and rewards for this episode
+      epx = np.vstack(xs)
+      eph = np.vstack(hs)
+      epdlogp = np.vstack(dlogps)
+      epr = np.vstack(drs)
+      xs,hs,dlogps,drs = [],[],[],[] # reset array memory
 
-    for k in model:
-      grad_buffer[k] += grad[k] # accumulate gradient over batch
+      # compute the discounted reward
+      discounted_epr = discounted_rewards(epr)
+      # standardize the rewards to be unit normal (helps control the gradient estimator variance)
+      discounted_epr -= np.mean(discounted_epr)
+      discounted_epr /= np.std(discounted_epr)
 
-    # perform rmsprop parameter update every batch_size episodes
-    # Background on gradient descent:
-    # Use backprob to calculate the gradients based on the loss function
-    # Update the weights of the model using learning rate alpha:
-    # W = W - alpha * dW
-    # Background on rmsprop:
-    # We want the gradients to oscillate less and move faster in the horizonal direction
-    # towards the minimum for a 2-D problem. Rmsprop works similarly as gradient descent 
-    # with momentum, which uses the exponentially weighted averages of the gradients
-    # in order to dampens out the oscillations.
-    # https://www.youtube.com/watch?v=_e-LFe_igno
-    # Rmsprop formula:
-    # S_dw = beta * S_dw + (1-beta) * dw^2
-    # W = W - alpha * dw / sqrt(S_dw)
+      epdlogp *= discounted_epr # modulate the gradient with advantage (PG magic happens right here.)
+      grad = policy_backward(epx, eph, epdlogp)
 
-    if episode_number % batch_size == 0:
-      for k,v in model.items():
-        g = grad_buffer[k] # gradient
-        rmsprop_cache[k] = decay_rate * rmsprop_cache[k] + (1 - decay_rate) * g**2
-        model[k] += learning_rate * g / (np.sqrt(rmsprop_cache[k]) + 1e-5) # 1e-5 is added to prevent diff by 0
-        grad_buffer[k] = np.zeros_like(v) # reset batch gradient buffer
+      for k in model:
+        grad_buffer[k] += grad[k] # accumulate gradient over batch
 
-    # book-keeping
-    running_reward = reward_sum if running_reward is None else running_reward * 0.99 + reward_sum * 0.01
-    print('Resetting env. Episode reward total was {}. Running mean: {}'.format(reward_sum, running_reward))
-    if episode_number % 100  == 0:
-      pickle.dump(model, open('save.p', 'wb'))
+      # perform rmsprop parameter update every BATCH_SIZE episodes
+      # Background on gradient descent:
+      # Use backprob to calculate the gradients based on the loss function
+      # Update the weights of the model using learning rate alpha:
+      # W = W - alpha * dW
+      # Background on rmsprop:
+      # We want the gradients to oscillate less and move faster in the horizonal direction
+      # towards the minimum for a 2-D problem. Rmsprop works similarly as gradient descent 
+      # with momentum, which uses the exponentially weighted averages of the gradients
+      # in order to dampens out the oscillations.
+      # https://www.youtube.com/watch?v=_e-LFe_igno
+      # Rmsprop formula:
+      # S_dw = beta * S_dw + (1-beta) * dw^2
+      # W = W - alpha * dw / sqrt(S_dw)
 
-    reward_sum = 0
-    observation = env.reset() # reset env
-    # For gym ~ 0.26
-    observation = observation[0]
-    prev_x = None
+      if episode_number % BATCH_SIZE == 0:
+        for k,v in model.items():
+          g = grad_buffer[k] # gradient
+          rmsprop_cache[k] = DECAY_RATE * rmsprop_cache[k] + (1 - DECAY_RATE) * g**2
+          model[k] += LEARNING_RATE * g / (np.sqrt(rmsprop_cache[k]) + 1e-5) # 1e-5 is added to prevent diff by 0
+          grad_buffer[k] = np.zeros_like(v) # reset batch gradient buffer
 
-  if reward != 0: # Pong has either +1 or -1 reward exactly when the game ends.
-    print('Episode number {}: Game finished with reward: {}'.format(episode_number, reward) + ('' if reward == -1 else '!!!!!!!'))
+      # book-keeping
+      running_reward = reward_sum if running_reward is None else running_reward * 0.99 + reward_sum * 0.01
+      print('Resetting env. Episode reward total was {}. Running mean: {}'.format(reward_sum, running_reward))
+      if episode_number % 100  == 0:
+        pickle.dump(model, open('save.p', 'wb'))
+
+      reward_sum = 0
+      observation = env.reset() # reset env
+      # For gym ~ 0.26
+      observation = observation[0]
+      prev_x = None
+
+    if reward != 0: # Pong has either +1 or -1 reward exactly when the game ends.
+      print('Episode number {}: Game finished with reward: {}'.format(episode_number, reward) + ('' if reward == -1 else '!!!!!!!'))
 
